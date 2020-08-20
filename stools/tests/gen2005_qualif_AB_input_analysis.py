d629a215d895f2d508db08c10f067aa72d12cab3
olgare <olga.reinauer@alpeslasers.ch>
2020-08-20 09:41:02 +0200

    modification1 au firmware: communiation protocol changed

---
+++ stools/tests/gen2005_qualif_AB_input_analysis.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created by Olga Reinauer
+
+Copyright 2019 Alpes Lasers SA, Saint-Blaise, Switzerland
+"""
+
+__author__ = 'olgare'
+__copyright__ = "Copyright 2019, Alpes Lasers SA"
+
+import logging
+
+logger = logging.getLogger(__name__)
